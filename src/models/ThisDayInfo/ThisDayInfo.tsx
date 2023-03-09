16e0d7bfdabadd3e4dd907303543f1f3356b2220
Vitalik <vitalik.kolodko.dev@yandex.ru>
2023-03-09 15:32:24 +0300

    thisDayInfo

--- src/models/ThisDayInfo/ThisDayInfo.tsx
+++ src/models/ThisDayInfo/ThisDayInfo.tsx
@@ -15,38 +15,74 @@ interface ThisDayInfoProps {
    winddirection: number | undefined,
 }
 
-export const ThisDayInfo = ({ pressure, temperature, precipitation, wind, apparent_temperature, winddirection }: ThisDayInfoProps) => {
+export const ThisDayInfo = ({ pressure, temperature, apparent_temperature, precipitation, wind, winddirection }: ThisDayInfoProps) => {
 
-   const [directionwind, setDirectionWind] = useState('')
+   const [directionWind, setDirectionWind] = useState('')
+   const [speedWind, setSpeedWind] = useState('')
+   const [precipitationInterest, setPrecipitationInterest] = useState('')
 
    useEffect(() => {
-      if (winddirection) {
-         if (winddirection >= 337.5 && winddirection <= 360 || winddirection <= 22.5) {
-            setDirectionWind('север')
+      if (winddirection !== undefined) {
+         if ((winddirection >= 337.5 && winddirection <= 360) || (winddirection <= 22.5)) {
+            setDirectionWind('северный')
          }
-         if (winddirection <= 67.5 && winddirection >= 22.5) {
-            setDirectionWind('северо-восток')
+         if (winddirection <= 67.5 && winddirection > 22.5) {
+            setDirectionWind('северо-восточный')
          }
-         if (winddirection <= 67.5 && winddirection >= 22.5) {
-            setDirectionWind('восток')
+         if (winddirection <= 112.5 && winddirection > 67.5) {
+            setDirectionWind('восточный')
          }
-         if (winddirection <= 67.5 && winddirection >= 22.5) {
-            setDirectionWind('юго-восток')
+         if (winddirection <= 157.5 && winddirection > 112.5) {
+            setDirectionWind('юго-восточный')
          }
-         if (winddirection <= 67.5 && winddirection >= 22.5) {
-            setDirectionWind('юг')
+         if (winddirection <= 202.5 && winddirection > 157.5) {
+            setDirectionWind('южный')
          }
-         if (winddirection <= 67.5 && winddirection >= 22.5) {
-            setDirectionWind('юг-запад')
+         if (winddirection <= 247.5 && winddirection > 202.5) {
+            setDirectionWind('юго-западный')
          }
-         if (winddirection <= 67.5 && winddirection >= 22.5) {
-            setDirectionWind('запад')
+         if (winddirection <= 292.5 && winddirection > 247.5) {
+            setDirectionWind('западный')
          }
-         if (winddirection <= 67.5 && winddirection >= 22.5) {
-            setDirectionWind('северо-запад')
+         if (winddirection <= 337.5 && winddirection > 292.5) {
+            setDirectionWind('северо-западый')
          }
       }
-   }, [winddirection])
+      if (wind !== undefined) {
+         if (Math.floor(wind) <= 5) {
+            setSpeedWind('cлабый')
+         }
+         if (Math.floor(wind) <= 14 && Math.floor(wind) > 6) {
+            setSpeedWind('умеренный')
+         }
+         if (Math.floor(wind) <= 24 && Math.floor(wind) > 15) {
+            setSpeedWind('сильный')
+         }
+         if (Math.floor(wind) <= 32 && Math.floor(wind) > 25) {
+            setSpeedWind('очень сильный')
+         }
+         if (Math.floor(wind) >= 33) {
+            setSpeedWind('ураганный')
+         }
+      }
+      if (precipitation !== undefined) {
+         if (precipitation <= 5) {
+            setPrecipitationInterest('без осадков')
+         }
+         if (precipitation <= 30 && precipitation > 6) {
+            setPrecipitationInterest('мало вероятно')
+         }
+         if (precipitation <= 60 && precipitation > 31) {
+            setPrecipitationInterest('возможно, можно взять зотник')
+         }
+         if (precipitation <= 80 && precipitation > 61) {
+            setPrecipitationInterest('осадки почти навердика зонтик точно нужен')
+         }
+         if (precipitation >= 81) {
+            setPrecipitationInterest('одназначно осадки')
+         }
+      }
+   }, [winddirection, wind, precipitation])
 
    return (
       <div className={styles.block__two}>
@@ -63,12 +99,12 @@ export const ThisDayInfo = ({ pressure, temperature, precipitation, wind, appare
          <div className={styles.block__two__inner}>
             < SelectorSvgOther svgName={'precipitation'} />
             <p>Осадки</p>
-            <p>{`${precipitation} Без осадков`}</p>
+            <p>{`${precipitation} % - ${precipitationInterest}`}</p>
          </div>
          <div className={styles.block__two__inner}>
             < SelectorSvgOther svgName={'wind'} />
             <p>Ветер</p>
-            {wind && <p>{`Скорость ${Math.floor(wind)} м/с ${directionwind}`}</p>}
+            {wind && <p>{`Скорость ${Math.floor(wind)} м/с ${directionWind} - ${speedWind}`}</p>}
          </div>
          <img src={cloud} alt="cloud" />
       </div>
